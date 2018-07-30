73ff5f09fc581806da3587cfa0b4768b960b44cd
Tilchin, Sergey <st250176@ncr.com>
2018-07-30 15:12:14 +0300

    htyht

--- githubprojects/githubprojects/Program.cs
+++ githubprojects/githubprojects/Program.cs
@@ -6,7 +6,7 @@ namespace githubprojects
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!"); 
         }
     }
 }
